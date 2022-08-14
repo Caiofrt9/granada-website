b7d9b48c94960778a799206828d975b5b50c2339
Caio F <silvacaio295@gmail.com>
2022-08-14 14:50:46 -0300

    presentation

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,28 @@
 import styles from './Home.css'
 
+import avatar from '../../assets/doleta.png'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className="container">
+      <div className="presentation">
+        <div className="presentation-text">
+          <h1>Atlética Granada</h1>
+          <p>
+            is simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry's standard dummy text ever since the
+            1500s, when an unknown printer took a galley of type and scrambled
+            it to make a type specimen book. It has survived not only five
+          </p>
+          <button>Saiba Mais</button>
+          <h2>Eu nao vim pra dar lucro, vim pra dar prejuizo</h2>
+        </div>
+        <div className="avatar-img">
+          <img src={avatar} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
